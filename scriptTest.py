8123c2888d968683501a8165a4b299687fb6b1ba
5umic <80717551+5umic@users.noreply.github.com>
2023-11-18 17:12:52 +0100

    Add files via upload

--- scriptTest.py
+++ scriptTest.py
@@ -3,14 +3,40 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+driver_path = '/Applications/chromedriver'
+url = 'https://www.sololearn.com/learn/courses/css-introduction/lesson/1934607001?p=1'  # replace with the URL of the website you're working with
+driver = webdriver.Chrome()
+driver.get(url)
+wait = WebDriverWait(driver, 10)
 
-def perform_script():
+def click_single_option():
 
+    # Wait until the container is present
+    container = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "sl-single-choice-answer")))
+    
+    # Find all single choice items inside the container
+    single_choice_items = container.find_elements(By.CLASS_NAME, 'sl-single-choice-item')
+
+    if single_choice_items:
+        # Wait until the first single choice item becomes clickable
+        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'sl-single-choice-item')))
+        
+        # Click on the first single choice item
+        single_choice_items[0].click()
+    else:
+        print("No single choice items found in the container.")
+    
+
+def accept_cookies():
+    cookieButton = wait.until(EC.presence_of_element_located((By.ID, "CybotCookiebotDialogBodyLevelButtonAccept")))
+    cookieButton.click()
+
+def click_continue_button():
     continueButton = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/div/main/div[1]/div/div/footer/div/button[2]')))
     continueButton.click()
 
 
-    # Click the "Continue" button
+def click_elements_in_container():
     container_xpath = '//*[@id="main"]/div/main/div[1]/div/div/div[1]/div/div/div/div[3]/div/div[2]'
     container = wait.until(EC.presence_of_element_located((By.XPATH, container_xpath)))
 
@@ -28,30 +54,33 @@ def watch_url_change(current_url):
         if new_url != current_url:
             print(f"URL changed to: {new_url}")
             current_url = new_url
-            perform_script()  # Rerun the script when the URL changes 
+            loginFunction()  # Rerun the script when the URL changes 
+            accept_cookies()
+            click_continue_button()
+            click_elements_in_container()
+            click_single_option()
+            click_continue_button()
 
 
-driver_path = '/Applications/chromedriver'
-url = 'https://www.sololearn.com/learn/courses/css-introduction/lesson/1934607001?p=1'  # replace with the URL of the website you're working with
-driver = webdriver.Chrome()
-driver.get(url)
-wait = WebDriverWait(driver, 10)
 
-emailVar = wait.until(EC.presence_of_element_located((By.ID, "email")))
-passVar = wait.until(EC.presence_of_element_located((By.ID, "password")))
 
-emailVar.send_keys("")#insert ur login info
-passVar.send_keys("")#insert ur password
+def loginFunction():
 
-signin_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "sol-button-primary")))
-signin_button.click()
+    emailVar = wait.until(EC.presence_of_element_located((By.ID, "email")))
+    passVar = wait.until(EC.presence_of_element_located((By.ID, "password")))
 
-cookieButton = wait.until(EC.presence_of_element_located((By.ID, "CybotCookiebotDialogBodyLevelButtonAccept")))
-cookieButton.click()
+    emailVar.send_keys("h23vehsu@du.se")#insert ur login info
+    passVar.send_keys("Horunge123")#insert ur password
 
-perform_script()
+    signin_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "sol-button-primary")))
+    signin_button.click()
+
+
+
+
 
 watch_url_change(driver)
 
 input("Press Enter to close the browser...")
 driver.quit()
+
